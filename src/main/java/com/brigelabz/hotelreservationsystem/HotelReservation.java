86426445caeb4ca1c9d8f0c988309e38209b8416
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 22:07:39 +0530

    REFACTORED : method to find cheapest and best rated hotel

--- src/main/java/com/brigelabz/hotelreservationsystem/HotelReservation.java
+++ src/main/java/com/brigelabz/hotelreservationsystem/HotelReservation.java
@@ -1,14 +1,18 @@
 package com.brigelabz.hotelreservationsystem;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 
 
 public class HotelReservation{
-	int weekdays =0, weekends =0;
+	int numOfWeekends =0, numOfWeekdays =0;
 	LinkedList<HotelModel> hotelList = new LinkedList<HotelModel>();
 	public boolean addHotel(String hotelName,double weekDayRates,double weekEndRates ,int rating)
 	{
@@ -20,34 +24,42 @@ public class HotelReservation{
 		
 	}
 
-public List<HotelModel> getCheapestHotel(LocalDate startDate, LocalDate endDate) {
-	int numberOfDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
-   
-    
-	do {
-		if(startDate.getDayOfWeek().equals("SUNDAY")||startDate.getDayOfWeek().equals("SATURDAY"))
-			weekends++;
-		else
-		{
-			weekdays++;
-		}
-        startDate = startDate.plusDays(1);
- 
-		} while(startDate.compareTo(endDate) !=0);
+public List<HotelModel> getCheapestHotel(String date1, String date2) {
 		
-		final double cheapestPrice = hotelList.stream()
-				.mapToDouble(hotel -> ((hotel.getWeekEndRates()*weekends) + hotel.getWeekDayRates()*weekdays))
-				.min()
-				.orElse(Double.MAX_VALUE);
-				System.out.println( "the minimum price is "+cheapestPrice);
-
-		LinkedList<HotelModel> cheapestHotel = hotelList.stream()
-				.filter(hotel -> (hotel.getWeekEndRates()*weekends + hotel.getWeekDayRates()*weekdays) == cheapestPrice)
-				.collect(Collectors.toCollection(LinkedList::new));
-		return cheapestHotel;
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy");
+		Calendar startDate = Calendar.getInstance(), endDate = Calendar.getInstance();
+		try {
+			startDate.setTime(sdf.parse(date1));
+			endDate.setTime(sdf.parse(date2));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	
+		do {
+			if(startDate.get(Calendar.DAY_OF_WEEK) == 0 || startDate.get(Calendar.DAY_OF_WEEK) == 6) numOfWeekends++;
+			else numOfWeekdays++;
+			startDate.add(Calendar.DATE,1);
+		} while(startDate.compareTo(endDate) <=0);
+		
+		HotelModel cheapestHotel = hotelList.stream()
+							.min((h1,h2) -> h1.getPrice(numOfWeekdays,numOfWeekends).compareTo(h2.getPrice(numOfWeekdays,numOfWeekends)))
+							.orElse(null);
+		
+		double cheapestPrice = cheapestHotel.getPrice(numOfWeekdays,numOfWeekends);
+		System.out.println("the minimum price is : "+cheapestPrice);
+		Predicate<HotelModel> isMinimum = (hotel) -> (hotel.getPrice(numOfWeekdays,numOfWeekends) == cheapestPrice)?true:false; 
+		List<HotelModel> cheapestHotels = hotelList.stream()
+									 .filter(isMinimum)
+									 .collect(Collectors.toList());
+		return cheapestHotels;
 	}
-
+	public HotelModel getCheapestAndBestRatedHotel(String date1, String date2) {
+		List<HotelModel> cheapestHotels = getCheapestHotel(date1,date2);
+		
+	return cheapestHotels.stream()
+		   .max((h1,h2) -> h1.getRating()-h2.getRating())
+		   .orElse(null);
+}
 
 public LinkedList<HotelModel> getHotelList() {
 	// TODO Auto-generated method stub
