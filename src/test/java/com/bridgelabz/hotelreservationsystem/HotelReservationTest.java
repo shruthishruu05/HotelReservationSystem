c9e2d82089d6f5959acb7db1945a69f837189b83
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 08:44:32 +0530

    ADDED : to test for added hotels is valid

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -1,5 +1,20 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.brigelabz.hotelreservationsystem.HotelModel;
+import com.brigelabz.hotelreservationsystem.HotelReservation;
+
 public class HotelReservationTest {
 
+	@Test
+	public void givenHotelDetails_WhenValuesEnteredAreCorrect_ShoulReturnTrue()
+	{
+		HotelReservation hotelReservationService = new HotelReservation();
+		HotelModel hotelModel = new HotelModel("Lakewood",  110, "General");
+		boolean isValidHotel1 = hotelReservationService.addHotel(hotelModel);
+		Assert.assertTrue(isValidHotel1);
+		
+	}
 }
