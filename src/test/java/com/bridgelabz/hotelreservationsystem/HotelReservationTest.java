87e779af8698da152c41925ef21224f6eaffc0cd
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 22:09:27 +0530

    REFACTORED : junit test cases to test findCheapestAndBestRatedHotel method

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -34,14 +34,28 @@ public class HotelReservationTest
 	}
 	
 	@Test
-	public void givenDate_find_CheapestHotel() {
+	public void givenDateRange_find_CheapestHotel() {
 		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel("Lakewood", 110,90,2);
-		hotelReservation.addHotel("Bridgewood", 150, 50,1);
-		hotelReservation.addHotel("Ridgewood",220,150,4);
-		LocalDate startDate = LocalDate.of(2021, 9, 10);
-		LocalDate endDate = LocalDate.of(2021, 9, 11);
-		List<HotelModel> hotel = hotelReservation.getCheapestHotel(startDate,endDate);
-		System.out.println("the hotel with cheapest price is "+hotel);
+		hotelReservation.addHotel("Lakewood",110.0,90.0,3);
+		hotelReservation.addHotel("Bridgewood",150.0,50.0,4);
+		hotelReservation.addHotel("Ridgewood",220.0,150.0,5);
+		
+		List<HotelModel> hotels = hotelReservation.getCheapestHotel("11-9-2020","12-9-2020");
+		System.out.println("the cheapest hotel is/are : ");
+		System.out.println(hotels);
+		System.out.println();
 	}
+	@Test
+	public void givenDateRange_find_CheapestAndBestRatedHotel() {
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("Lakewood",110.0,90.0,3);
+		hotelReservation.addHotel("Bridgewood",150.0,50.0,4);
+		hotelReservation.addHotel("Ridgewood",220.0,150.0,5);
+		
+		HotelModel hotel = hotelReservation.getCheapestAndBestRatedHotel("11-9-2020","12-9-2020");
+		System.out.println("the cheapest and best rated hotel is : ");
+		System.out.println(hotel);
+		System.out.println();
+	}
+	
 }
