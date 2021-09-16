1853cbd8c1815b128bc75e6c48630dcb809171f4
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 16:30:56 +0530

    REFACTORED : junit test method to check for cheapest hotel price

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -15,18 +15,18 @@ public class HotelReservationTest
 	public void addedHotel_WhenProper_ShouldReturnTrue()
 	{
 		HotelReservation hotelReservation = new HotelReservation();
-		boolean result = hotelReservation.addHotel("Lakewood", 300, 3);
-		boolean result1  = hotelReservation.addHotel("Bridgewood", 400, 5);
-		boolean result2 = hotelReservation.addHotel("Ridgewood", 500, 5);
+		boolean result = hotelReservation.addHotel("Lakewood", 110,90,2);
+		boolean result1  = hotelReservation.addHotel("Bridgewood", 150, 50,1);
+		boolean result2 = hotelReservation.addHotel("Ridgewood",220,150,4);
 		Assert.assertTrue(result);
 	}
 	
 	@Test
 	public void givenDate_find_CheapestHotel() {
 		HotelReservation hotelManagement = new HotelReservation();
-		hotelManagement.addHotel("Lakewood", 300, 3);
-		hotelManagement.addHotel("Bridgewood", 400, 5);
-		hotelManagement.addHotel("Ridgewood", 500, 5);
+		hotelManagement.addHotel("Lakewood", 110,90,2);
+		hotelManagement.addHotel("Bridgewood", 150, 50,1);
+		hotelManagement.addHotel("Ridgewood",220,150,4);
 		LocalDate startDate = LocalDate.of(2021, 9, 10);
 		LocalDate endDate = LocalDate.of(2021, 9, 11);
 		HotelModel hotel = hotelManagement.getCheapestHotel(startDate, endDate);
