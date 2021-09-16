da6ed1bfd1f8a1d1b3393f10701599f6973afc8b
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 19:40:46 +0530

    REFACTORED : returning cheapest hotel for a given date range

--- src/main/java/com/brigelabz/hotelreservationsystem/HotelReservation.java
+++ src/main/java/com/brigelabz/hotelreservationsystem/HotelReservation.java
@@ -1,15 +1,22 @@
 package com.brigelabz.hotelreservationsystem;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Period;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.Comparator;
 import java.util.stream.Collectors;
+import java.text.ParseException;
+import java.time.DayOfWeek;
+
 
 public class HotelReservation{
+	int weekdays =0, weekends =0;
 	LinkedList<HotelModel> hotelList = new LinkedList<HotelModel>();
 	public boolean addHotel(String hotelName,double weekDayRates,double weekEndRates ,int rating)
 	{
@@ -21,9 +28,30 @@ public class HotelReservation{
 		
 	}
 
-	public HotelModel getCheapestHotel(LocalDate startDate, LocalDate endDate) {
-		Period period = Period.between(startDate, endDate);
-		int noOfDaysBetween = (int)ChronoUnit.DAYS.between(startDate, endDate);
-		return hotelList.stream().min((h1,h2) -> h1.getPrice(noOfDaysBetween).compareTo(h2.getPrice(noOfDaysBetween))).orElse(null);
+public List<HotelModel> getCheapestHotel(LocalDate startDate, LocalDate endDate) {
+	int numberOfDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
+   
+    
+	do {
+		if(startDate.getDayOfWeek().equals("SUNDAY")||startDate.getDayOfWeek().equals("SATURDAY"))
+			weekends++;
+		else
+		{
+			weekdays++;
+		}
+        startDate = startDate.plusDays(1);
+ 
+		} while(startDate.compareTo(endDate) !=0);
+		
+		final double cheapestPrice = hotelList.stream()
+				.mapToDouble(hotel -> ((hotel.getWeekEndRates()*weekends) + hotel.getWeekDayRates()*weekdays))
+				.min()
+				.orElse(Double.MAX_VALUE);
+
+		LinkedList<HotelModel> cheapestHotel = hotelList.stream()
+				.filter(hotel -> (hotel.getWeekEndRates()*weekends + hotel.getWeekDayRates()*weekdays) == cheapestPrice)
+				.collect(Collectors.toCollection(LinkedList::new));
+		return cheapestHotel;
+
 	}
 }
