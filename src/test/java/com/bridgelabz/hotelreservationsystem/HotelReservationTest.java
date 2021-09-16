cb674f8edc0732199832ea731de3e3df8831e368
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 15:58:43 +0530

    REFACTORED : to test for the cheapest hotel

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -1,4 +1,6 @@
 package com.bridgelabz.hotelreservationsystem;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -6,15 +8,28 @@ import org.junit.Test;
 import com.brigelabz.hotelreservationsystem.HotelModel;
 import com.brigelabz.hotelreservationsystem.HotelReservation;
 
-public class HotelReservationTest {
 
+public class HotelReservationTest
+{
 	@Test
-	public void givenHotelDetails_WhenValuesEnteredAreCorrect_ShoulReturnTrue()
+	public void addedHotel_WhenProper_ShouldReturnTrue()
 	{
-		HotelReservation hotelReservationService = new HotelReservation();
-		HotelModel hotelModel = new HotelModel("Lakewood",  110, "General");
-		boolean isValidHotel1 = hotelReservationService.addHotel(hotelModel);
-		Assert.assertTrue(isValidHotel1);
-		
+		HotelReservation hotelReservation = new HotelReservation();
+		boolean result = hotelReservation.addHotel("Lakewood", 300, 3);
+		boolean result1  = hotelReservation.addHotel("Bridgewood", 400, 5);
+		boolean result2 = hotelReservation.addHotel("Ridgewood", 500, 5);
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenDate_find_CheapestHotel() {
+		HotelReservation hotelManagement = new HotelReservation();
+		hotelManagement.addHotel("Lakewood", 300, 3);
+		hotelManagement.addHotel("Bridgewood", 400, 5);
+		hotelManagement.addHotel("Ridgewood", 500, 5);
+		LocalDate startDate = LocalDate.of(2021, 9, 10);
+		LocalDate endDate = LocalDate.of(2021, 9, 11);
+		HotelModel hotel = hotelManagement.getCheapestHotel(startDate, endDate);
+		System.out.println("the hotel with cheapest price is "+hotel);
 	}
 }
