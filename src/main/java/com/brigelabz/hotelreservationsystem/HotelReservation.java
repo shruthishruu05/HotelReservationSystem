0d927c86a778d8a5bf1c1f1c948777aba59ba428
shruthishruu05 <shruthi.m@perfios.com>
2021-09-16 08:17:29 +0530

    ADDED : welcome message

---
+++ src/main/java/com/brigelabz/hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,8 @@
+package com.brigelabz.hotelreservationsystem;
+
+public class HotelReservation {
+public static void main(String [] args)
+{
+	System.out.println("Welcome to hotel reservation");
+}
+}
---
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationTest {
+
+}
